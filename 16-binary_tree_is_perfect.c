567ca4c79e0245f543db1d0f500acfa76d5cf6d3
FrancoDoldan0 <francodoldan23@gmail.com>
2023-09-02 17:26:35 -0700

    add task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -5,20 +5,20 @@
 /**
 *binary_tree_is_perfect - check if a tree is perfect
 *@tree: pointer to the root
-*Return: 0 
+*Return: 0
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return (0);
-    }
-    if (binary_tree_size(tree->right) == binary_tree_size(tree->left))
-    {   
-        if (binary_tree_is_full(tree) && !binary_tree_balance(tree))
-        {
-            return (1);
-        }
-    }
-    return (0);
+	if (!tree)
+	{
+		return (0);
+	}
+	if (binary_tree_size(tree->right) == binary_tree_size(tree->left))
+	{
+		if (binary_tree_is_full(tree) && !binary_tree_balance(tree))
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
